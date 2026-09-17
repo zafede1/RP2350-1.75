25302c789275f9db4df79b9d103b221407238f57
Fede <shinbansho100@gmail.com>
2026-09-17 23:17:36 +0200

    RP2350: evita secondo framebuffer e aggiungi compatibilità Canvas

--- tama_gfx.h
+++ tama_gfx.h
@@ -6,18 +6,15 @@
 class TamaGFX : public Adafruit_GFX {
 public:
   TamaGFX();
+  TamaGFX(int16_t w, int16_t h, void *unusedPanel);
   bool begin(uint32_t unused = 0);
   void flush();
   void setBrightness(uint8_t brightness255);
+  void setUTF8Print(bool enabled) { (void)enabled; }
   void drawPixel(int16_t x, int16_t y, uint16_t color) override;
   void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color) override;
   void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color) override;
   void fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color) override;
   void fillScreen(uint16_t color) override;
-private:
-  static constexpr int W = AMOLED_1IN75_WIDTH;
-  static constexpr int H = AMOLED_1IN75_HEIGHT;
-  uint16_t framebuffer[W * H];
 };
-
 extern TamaGFX tamaGfx;
