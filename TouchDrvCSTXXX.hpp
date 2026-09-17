17312836b0211c04fdda6bbd0205b71166fd66be
Fede <shinbansho100@gmail.com>
2026-09-17 23:18:42 +0200

    RP2350: allinea la firma begin() del touch

--- TouchDrvCSTXXX.hpp
+++ TouchDrvCSTXXX.hpp
@@ -1,22 +1,19 @@
 #pragma once
 #ifdef ARDUINO_ARCH_RP2040
 #include "CST9217.h"
-
 class TouchDrvCST92xx {
 public:
   void setPins(int8_t resetPin, int8_t intPin) { reset_ = resetPin; int_ = intPin; }
-  bool begin(TwoWire &, uint8_t = CST9217_I2C_ADDR) { CST9217_Init(); return true; }
+  bool begin(TwoWire &, uint8_t = CST9217_I2C_ADDR, int = -1, int = -1) { CST9217_Init(); return true; }
   void reset() { CST9217_Reset(); }
   void setMaxCoordinates(uint16_t, uint16_t) {}
   void setMirrorXY(bool mx, bool my) { mirrorX_ = mx; mirrorY_ = my; }
   int getPoint(int16_t *x, int16_t *y, uint8_t = 1) {
     if (!CST9217_Read_Data() || !CST9217.points || !CST9217.data[0].valid) return 0;
-    int16_t px = (int16_t)CST9217.data[0].x;
-    int16_t py = (int16_t)CST9217.data[0].y;
+    int16_t px = (int16_t)CST9217.data[0].x, py = (int16_t)CST9217.data[0].y;
     if (mirrorX_) px = 465 - px;
     if (mirrorY_) py = 465 - py;
-    *x = px; *y = py;
-    return 1;
+    *x = px; *y = py; return 1;
   }
 private:
   int8_t reset_ = -1, int_ = -1;
