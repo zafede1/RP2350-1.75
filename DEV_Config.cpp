64b7460714ddd5f40630e0e2b106e6a202a2adb9
Fede <shinbansho100@gmail.com>
2026-09-17 23:42:51 +0200

    Keep RP2350 clock within board rating

--- DEV_Config.cpp
+++ DEV_Config.cpp
@@ -12,26 +12,41 @@ void DEV_Delay_ms(uint32_t ms) { delay(ms); }
 void DEV_Delay_us(uint32_t us) { delayMicroseconds(us); }
 
 void DEV_I2C_Write_Byte(uint8_t addr, uint8_t reg, uint8_t value) {
-  Wire1.beginTransmission(addr); Wire1.write(reg); Wire1.write(value); Wire1.endTransmission();
+  Wire1.beginTransmission(addr);
+  Wire1.write(reg);
+  Wire1.write(value);
+  Wire1.endTransmission();
 }
+
 uint8_t DEV_I2C_Read_Byte(uint8_t addr, uint8_t reg) {
-  Wire1.beginTransmission(addr); Wire1.write(reg); Wire1.endTransmission(false);
+  Wire1.beginTransmission(addr);
+  Wire1.write(reg);
+  Wire1.endTransmission(false);
   if (Wire1.requestFrom(addr, (uint8_t)1) != 1) return 0;
   return Wire1.read();
 }
+
 void DEV_I2C_Read_nByte(uint8_t addr, uint8_t reg, uint8_t *data, uint32_t len) {
-  Wire1.beginTransmission(addr); Wire1.write(reg); Wire1.endTransmission(false);
+  Wire1.beginTransmission(addr);
+  Wire1.write(reg);
+  Wire1.endTransmission(false);
   uint32_t got = Wire1.requestFrom(addr, (uint8_t)len);
   for (uint32_t i = 0; i < got && i < len; ++i) data[i] = Wire1.read();
   for (uint32_t i = got; i < len; ++i) data[i] = 0;
 }
-void DEV_IRQ_SET(uint gpio, uint32_t events, gpio_irq_callback_t callback) { gpio_set_irq_enabled_with_callback(gpio, events, true, callback); }
+
+void DEV_IRQ_SET(uint gpio, uint32_t events, gpio_irq_callback_t callback) {
+  gpio_set_irq_enabled_with_callback(gpio, events, true, callback);
+}
 
 uint8_t DEV_Module_Init(void) {
   Serial.begin(115200);
   delay(100);
-  set_sys_clock_khz(PLL_SYS_KHZ, true);
-  gpio_init(TOUCH_RST_PIN); gpio_set_dir(TOUCH_RST_PIN, GPIO_OUT);
+  // Waveshare specifica il funzionamento fino a 150 MHz per RP2350A.
+  set_sys_clock_khz(150 * 1000, true);
+
+  gpio_init(TOUCH_RST_PIN);
+  gpio_set_dir(TOUCH_RST_PIN, GPIO_OUT);
 
   dma_tx = dma_claim_unused_channel(true);
   c = dma_channel_get_default_config(dma_tx);
@@ -46,6 +61,7 @@ uint8_t DEV_Module_Init(void) {
   Wire1.begin();
   return 0;
 }
+
 void DEV_Module_Exit(void) {
   if (dma_tx) dma_channel_abort(dma_tx);
   Wire1.end();
