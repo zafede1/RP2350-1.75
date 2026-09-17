9d70ce19e61b4673e7258acc39866d76edf8cdc2
Fede <shinbansho100@gmail.com>
2026-09-18 01:28:53 +0200

    RP2350: use AMOLED dimensions in GFX framebuffer

--- tama_gfx.cpp
+++ tama_gfx.cpp
@@ -5,8 +5,8 @@
 static uint16_t framebuffer[AMOLED_1IN75_WIDTH * AMOLED_1IN75_HEIGHT];
 TamaGFX tamaGfx;
 
-TamaGFX::TamaGFX() : Adafruit_GFX(W, H) {}
-TamaGFX::TamaGFX(int16_t, int16_t, void *) : Adafruit_GFX(W, H) {}
+TamaGFX::TamaGFX() : Adafruit_GFX(AMOLED_1IN75_WIDTH, AMOLED_1IN75_HEIGHT) {}
+TamaGFX::TamaGFX(int16_t, int16_t, void *) : Adafruit_GFX(AMOLED_1IN75_WIDTH, AMOLED_1IN75_HEIGHT) {}
 
 bool TamaGFX::begin(uint32_t) {
   static bool initialized = false;
@@ -27,7 +27,7 @@ void TamaGFX::setBrightness(uint8_t brightness255) { AMOLED_1IN75_SetBrightness(
 
 void TamaGFX::drawPixel(int16_t x, int16_t y, uint16_t color) {
   if (x < 0 || y < 0 || x >= _width || y >= _height) return;
-  framebuffer[(size_t)y * W + x] = color;
+  framebuffer[(size_t)y * AMOLED_1IN75_WIDTH + x] = color;
 }
 void TamaGFX::drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color) {
   if (y < 0 || y >= _height || w <= 0) return;
@@ -56,4 +56,4 @@ void TamaGFX::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t colo
     for (int16_t xx = 0; xx < w; ++xx) p[xx] = color;
   }
 }
-void TamaGFX::fillScreen(uint16_t color) { fillRect(0, 0, W, H, color); }
+void TamaGFX::fillScreen(uint16_t color) { fillRect(0, 0, AMOLED_1IN75_WIDTH, AMOLED_1IN75_HEIGHT, color); }
