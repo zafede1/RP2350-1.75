b4d4cb18a0c3932d6169de6e10fa587e71bbb982
Fede <shinbansho100@gmail.com>
2026-09-19 00:50:10 +0200

    Fix audio header comment

--- audio.h
+++ audio.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <stdint.h>
 
-// Effetti sonori del gioco. Su RP2350 vengono riprodotti in modo sincrono\n// per pochi millisecondi.
+// Effetti sonori del gioco. Su RP2350 vengono riprodotti in modo sincrono.
+// La microSD usa SPI0 sui GPIO 18/19/20/21 e non condivide i pin con I2S.
 enum Sfx : uint8_t {
   SFX_TAP = 0,
   SFX_EAT,
