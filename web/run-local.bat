594d04e6f40557937969ab18ae9ec5e4d2f3fdb6
Fede <shinbansho100@gmail.com>
2026-09-18 03:25:01 +0200

    Make Windows local launcher robust

--- web/run-local.bat
+++ web/run-local.bat
@@ -1,4 +1,9 @@
 @echo off
 cd /d "%~dp0"
 start "" http://localhost:8000/
-python -m http.server 8000
+where py >nul 2>&1
+if %errorlevel%==0 (
+  py -m http.server 8000
+) else (
+  python -m http.server 8000
+)
